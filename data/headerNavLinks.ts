b16bafc06b2a80e81717019a3e66edd4457ec71c
BMO <meow@nermalcat69.dev>
2025-12-23 18:11:37 +0530

    iterate setup

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,16 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/available-locations', title: 'Locations' },
+  { href: '/products', title: 'Products' },
+  { href: 'https://b2b.graycup.in', title: 'Bulk Store' },
+]
+
+const headerTopNavLinks = [
+  { href: 'https://graycup.org', title: 'Contact Us' },
+  { href: 'https://graycup.in', title: 'Retail Online Store' },
+  { href: 'https://graycup.org', title: 'Powered by Gray Cup' },
+]
+
+export default headerNavLinks
+export { headerTopNavLinks }
