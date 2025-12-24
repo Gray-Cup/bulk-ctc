202af23409abaa18d0bbce8fa4abe45d57deead1
BMO <meow@nermalcat69.dev>
2025-12-24 15:58:27 +0530

    init cloudflare

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  // Set your site's URL
+  site: 'https://astro-i18n-starter.pages.dev',
+
+  i18n: {
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
+
+  integrations: [
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: DEFAULT_LOCALE_SETTING,
+        locales: Object.fromEntries(
+          Object.entries(LOCALES_SETTING).map(
+            ([key, value]) => [key, value.lang ?? key]
+          )
+        ),
+      },
+    })
+  ],
+
+  adapter: cloudflare(),
+});
